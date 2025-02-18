c4bb8a754b7993cd1547e498176f9664cf99d7d7
Jessica Childress <jessicachildress@jessicas-air.lan>
2025-02-17 19:35:21 -0800

    create basic files

---
+++ 001_Basic_Agent.py
@@ -0,0 +1,8 @@
+"""
+Owner: Jessica Childress
+Description: This file will be used to create an AI agent using a connection to Open AI. 
+Sources: 
+    https://youtu.be/bZzyPscbtI8?si=Z5Ih_38aoyp__Y6E (Building AI Agents in Pure Python)
+    https://www.anthropic.com/research/building-effective-agents (Building Effective Agents)
+"""
+import anthropic
